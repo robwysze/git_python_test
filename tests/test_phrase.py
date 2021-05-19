4f32e74b0d3a41b21d6ffd9b6b1bbc95bb648def
Robert Wyszecki <r.wyszecki@toucan-systems.pl>
2021-05-19 23:34:35 +0200

    clean code

--- tests/test_phrase.py
+++ tests/test_phrase.py
@@ -1,11 +1,4 @@
 import pytest
-# import sys
-# import os.path
-#
-# sys.path.append(
-#     os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir))
-# )
-# from git_python_test.phrase import Phrase
 from git_python_test.phrase import Phrase
 
 
